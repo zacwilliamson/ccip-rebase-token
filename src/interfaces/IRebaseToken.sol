0ef73be074b1310b3a890b1fe3578b414170bb5a
supersakana <zacwilliamson@icloud.com>
2025-02-26 19:36:49 -0600

    added vault and token pool deployer

--- src/interfaces/IRebaseToken.sol
+++ src/interfaces/IRebaseToken.sol
@@ -8,4 +8,5 @@ interface IRebaseToken {
     function balanceOf(address account) external view returns (uint256);
     function getUserInterestRate(address _user) external returns (uint256);
     function getInterestRate() external view returns (uint256);
+    function grantMintAndBurnRole(address account) external;
 }
